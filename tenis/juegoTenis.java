b9156158f8ed88b1a47bc2ed8964aa982c2b1de0
marco saporiti <saporiti.marco.43792@gmail.com>
2024-09-23 22:13:09 -0300

    primer test verde

--- tenis/juegoTenis.java
+++ tenis/juegoTenis.java
@@ -1,3 +1,10 @@
-package juego;
-public class JuegoTenis {
+public class juegoTenis {
+
+    public juegoTenis(String jugador1, String jugador2) {
+
+    }
+
+    public int obtenerPuntaje(String jugador) {
+        return 0;
+    }
 }
---
+++ tenis/juegoTenisTest.java
@@ -0,0 +1,20 @@
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class juegoTenisTest {
+
+    private juegoTenis juego;
+
+    @BeforeEach
+    public void setUp() {
+        juego = new juegoTenis("Jugador 1", "Jugador 2");
+    }
+
+    @Test
+    public void losJugadoresEmpiezanConCeroPuntos() {
+        assertEquals(0, juego.obtenerPuntaje("Jugador 1"));
+        assertEquals(0, juego.obtenerPuntaje("Jugador 2"));
+    }
+}
